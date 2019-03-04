828756c58bf043c7fc843367419a41cc84915e08
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-04 17:27:06 -0500

    first question added and buttons populate with choices

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,13 @@
 // variable here
+var questions = ["Who marries the receptionist?"];
+var choices1 = ["Dwight", "Jim", "Toby", "Michael"];
+var questionCount = 0;
+
+$(document).ready(function() {
+    $("#question").text(questions[questionCount]);
+    for (i = 0; i < choices1.length; i++) {
+        var button = $("<button>");
+        button.text(choices1[i]);
+        $("#choices").append(button);
+    }
+});
